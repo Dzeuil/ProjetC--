cfcc267dddd18890b32c0b947e5c546026465447
Vincent <vincent.maisonnet@laposte.net>
2023-12-11 21:44:38 +0100

    Mauvaise version du fichier

--- graph.hpp
+++ graph.hpp
@@ -72,65 +72,6 @@ class Graph {
             system("dot -Tpng graphe.dot > graph.png");
         }
 
-        // Dijkstra algorithm
-        void shortestPath(int start) {   // The set is filled with the vertex number that is being processed now
-            Visited[start]=true;
-            cout<<"\nLe plus court chemin de "<<start<<" vers chaque sommet est : \n";
-            // A priority queue is used to get vertices in the order of their distance from source vertex.
-            priority_queue<pair<float,int>, vector<pair<float,int>>, greater<pair<float,int>>> p
-            Q;  // Q contains a pair of distance and vertex number
-            Q.push({0,start});
-            while(!Q.empty()) {
-                int u = Q.top().second;     // Pick the smallest distance vertex from Q[]
-                Q.pop();      // Remove the picked vertex from Q[]
-                Edge& edg=vertice[u].edges[0];
-                float weight=edg.val;
-                for (int i = 1; i < vertice[u].edges.size(); i++) {
-                    // If not visited then mark it as visited and enqueue it
-                    if (!Visited[edg.num]) {
-                        Visited[edg.num] = true;
-                        float w = edg.val;
-                        edg=vertice[u].edges[i];
-                        dist[edg.num] = min(dist[u] + edg.val, dist[edg.num]);
-                        Q.push({dist[edg.num], edg.num});
-                    }
-                    else if (dist[u]+weight < dist[edg.num]) {
-                        // Relaxing the edge
-                        dist[edg.num] = dist[u] + weight;
-                        pred[edg.num] = u;
-                    }
-                }
-            }
-        }
-        
-        // Function to print the constructed distance array
-        void printShortestPath(int n, int st) {
-            cout << "Distance from " << st << " to all other vertices are:\n";
-            for (int i = 0; i < V; i++)
-            cout << dist[i] << " ";
-            cout << endl;
-            /*
-            This function prints shortest path from src to dest.
-            It uses parent[] to build the path.
-            */
-            printPath(st);
-        }
-
-        // Print the path
-        void printPath(int dest) {
-            Stack s; // Create a stack
-            int temp = dest; // Set the destination city as the last node
-            while (temp != -1) { // Run till the source city is reached
-                s.push(temp); // Push the current city to the stack
-                temp = pred[temp]; // Get the predecessor of the current city
-            }
-            printf("\n%d",dest);
-            // All cities must be printed in reverse order
-            while (!s.isEmpty()) {
-                printf(" <- %d",s.pop());
-            }
-        }
-
         Vertex& get_vertex(int num) {
             for(Vertex &vertex : graph) {
                 if(vertex.num == num) {
